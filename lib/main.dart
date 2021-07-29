390f8944899e50ce04bf6c0958a221903a504ba4
Victor Rivera <verm2405@gmail.com>
2021-07-29 19:29:12 -0400

    Display card list

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+Future main() async {
+  await dotenv.load(fileName: '.env');
   runApp(new MaterialApp(title: "Clean Framework Movies", home: Home()));
 }
 
@@ -11,6 +16,7 @@ class Home extends StatelessWidget {
       appBar: new AppBar(
         title: new Text("Clean Movies"),
       ),
+      body: MoviesList(),
     );
   }
 }
@@ -23,10 +29,49 @@ class MoviesList extends StatefulWidget {
 }
 
 class MoviesListState extends State<MoviesList> {
+  // API Key for call. This should NOT live here
+  var apikey = dotenv.env['TMDB_API_KEY'];
+
+  List movies = [];
+
+  void fetchNowPlayingMovies() async {
+    final response = await http.get(Uri.parse(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=$apikey&language=en-US&page=1'));
+    final body = jsonDecode(response.body);
+
+    this.setState(() {
+      movies = body['results'];
+    });
+  }
+
+  @override
+  void initState() {
+    this.fetchNowPlayingMovies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      children: [
+        Expanded(
+          child: SizedBox(
+            height: 200,
+            child: ListView.builder(
+                itemCount: movies.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    height: 220,
+                    width: double.maxFinite,
+                    child: Card(
+                      elevation: 5,
+                      child: new Text(movies[index]['original_title']),
+                    ),
+                  );
+                }),
+          ),
+        )
+      ],
     );
   }
 }
